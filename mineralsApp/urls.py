1988554443cee7e235aba9473fc567aac76045cb
Rukin Danil <rukin2018d@yandex.ru>
2022-04-23 13:36:59 +0700

    index

---
+++ mineralsApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.load_index_page, name='index'),
+    # path('about', views.loadAboutPage, name='about_page'),
+    path('admin', views.load_minerals_page, name='admin'),
+    # path('city/<city_id>', views.loadCityPage),
+    # path('excursions', views.loadExcursionsPage, name='excursions_page'),
+    # path('excursion/<excursion_id>', views.loadExcursionPage),
+    # path('excursion/delete/<excursion_id>', views.deleteExcursion),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- mineralsApp/views.py
+++ mineralsApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from models import *
+
 
 # Create your views here.
+
+
+def load_minerals_page(request):
+    minerals = Mineral.objects.all()
+    return render(request, 'admin.html', {'minerals': minerals})
+
+
+def load_index_page(request):
+    return render(request, 'index.html')
+
